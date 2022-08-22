cf9c405b4468b26beec083c2c49f69cb5ab86b3f
Artur Petryk <petrikartur@gmail.com>
2022-08-22 20:12:20 +0300

    CPB-3: Add code linter && enable pre-commit hooks

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,7 +1,8 @@
-from .managers import UserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+from .managers import UserManager
 
 
 class User(PermissionsMixin, AbstractBaseUser):
