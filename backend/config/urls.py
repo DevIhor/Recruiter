2889cf467603296ecd6c01606b291bc116267f69
Vladyslav L <96374749+LV-coding@users.noreply.github.com>
2022-08-25 16:57:35 +0300

    minor fix ulrs.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,3 +10,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
