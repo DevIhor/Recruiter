341f01c0517cf209d4543d7c2f867fea55b47ddb
Dmytro Tymoshchenko <dietrich.kreze@gmail.com>
2022-08-31 17:37:29 +0300

    Moved settings to prod.py, used dummy settings in base

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -140,9 +140,5 @@ CORS_ALLOW_METHODS = list(default_methods) + []
 CORS_ALLOW_HEADERS = list(default_headers) + []
 
 # Email
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "emails")
