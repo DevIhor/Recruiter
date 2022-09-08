a69574f98a3293848454c0bef53b1aff611b0966
Artur Petryk <petrikartur@gmail.com>
2022-09-08 23:02:52 +0300

    Fix: tests

--- backend/apps/accounts/tests/test_user_reset_password.py
+++ backend/apps/accounts/tests/test_user_reset_password.py
@@ -1,6 +1,8 @@
 from unittest.mock import patch
 
+from apps.accounts.factories import UserFactory
 from django.contrib.auth import get_user_model
+from django_rest_passwordreset.models import ResetPasswordToken
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
@@ -14,9 +16,7 @@ class UserResetPasswordTestCase(APITestCase):
     def setUp(self) -> None:
         """Called before each single test. Create a new user."""
 
-        self.user = UserModel.objects.create(
-            email="testuser@ex.com", password="random_string", is_active=True
-        )
+        self.user = UserFactory.create()
         self.request_password_reset_url = reverse("password_reset:reset-password-request")
         self.confirm_password_reset_url = reverse("password_reset:reset-password-confirm")
 
@@ -39,6 +39,17 @@ class UserResetPasswordTestCase(APITestCase):
         data = {"email": "invalid_email_address@ex.com"}
         response = self.client.post(self.request_password_reset_url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_user_confirm_password_reset(self):
+        """Ensure user can set a new password with a valid token."""
+
+        token = ResetPasswordToken.objects.create(user=self.user)
+        data = {"token": token.key, "password": "new_password_string"}
+        response = self.client.post(self.confirm_password_reset_url, data, format="json")
+        self.user = UserModel.objects.get(email=self.user.email)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.user.check_password("1234567890"), False)
 
     def test_user_confirm_password_reset_fail(self):
         """Ensure user can't set a new password with an invalid token."""
